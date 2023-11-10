447adc6ff13340fec5cb600c0e1d88be38da3017
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-10 11:26:35 +0600

    Subtriction JS

--- operation.js
+++ operation.js
@@ -1,6 +1,14 @@
+// Addition JS
 var orangePrice = 20 ;
 var mangoPrice = 30 ;
 
 var omTotalPrice = (orangePrice + mangoPrice) ;
 
-console.log(omTotalPrice)
+console.log(omTotalPrice) ;
+// Subtraction JS
+var tkDilo = 100 ;
+var tkKhoroch = 70 ;
+
+var ferotAslo = (tkDilo - tkKhoroch) ;
+
+console.log(ferotAslo) ;
