de0e8a11101a0c1c5b7d04a446ca9a09f2f5075d
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-10 12:31:09 +0600

    short hand operation

---
+++ shorthand.js
@@ -0,0 +1,25 @@
+// shorthand applied oparation
+// variable maan:
+var apple = 20 ; 
+// variable declaration
+apple = 30 ;
+
+// Addition short hand
+apple += 20 ;
+
+console.log(apple) ;
+// subtraction Short hand
+
+apple -= 5 ;
+
+console.log(apple) ;
+// multiplication Short hand
+
+apple *= 10 ;
+
+console.log(apple) ;
+// Division Short Hand
+
+apple /= 5 ;
+
+console.log(apple) ;
