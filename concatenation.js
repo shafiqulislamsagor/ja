0be2c42e040dba56d4dc4fb5e7e4dc61a27178db
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-10 15:38:51 +0600

    concatenation practice

---
+++ concatenation.js
@@ -0,0 +1,26 @@
+// 1st practice:
+var firstName = "Shafiqul Islam" ;
+var lastName = "Sagor" ;
+var fullName = firstName + " " + lastName ;
+console.log(fullName);
+
+// 2nd practice:
+var bazar = 50;
+var total = "20";
+var bazarTotal = bazar + total ;
+console.log(bazarTotal);
+
+// 3rd practice:
+var point = "20" ;
+var point2 = "50" ;
+var pointTotal = point + point2;
+console.log(pointTotal);
+
+// 4th practice:
+var point1 = parseInt(point)
+console.log(point1)
+
+// 5th practice:
+var allItems = bazar + point1 ;
+console.log(allItems);
+
