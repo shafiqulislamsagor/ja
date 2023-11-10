abc32cc1f99543b6abb70a2c92027057d58bedf8
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-10 08:24:00 +0600

    module 17.4 string , numeric and bollean

--- variable.js
+++ variable.js
@@ -1,4 +1,11 @@
-// variable
+// variable numaric
 var ojon = 47 ;
 var money = 10;
 var age = 19; 
+// variable string
+var name =" Shafiqul Islam Sagor";
+var special =" Al Mujiza Rahman Habiba";
+var baby ="Sidratul Islam Safa"
+// variabla bollean
+var single = false;
+var realation = true;
