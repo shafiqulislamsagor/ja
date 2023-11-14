e43db24f8e2103599a17469991be51d65c1b89ff
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-14 22:17:18 +0600

    number

---
+++ number.js
@@ -0,0 +1,12 @@
+let num = 20 ;
+let numM = num.toString();
+console.log(numM);
+console.log(typeof(numM));
+
+let x = 123;
+let newM = x.toString();
+console.log(newM)
+
+let y = 123456;
+let srts = y.toString();
+console.log(srts)
