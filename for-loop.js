1f900c43b8e0fd703c371cbb34509db5647aa129
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-14 23:23:10 +0600

    for loop

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+const couple = ["Habiba" , "Sagor" ,"Mohona" , "Rahim" ,"Akhi" , "Arif" , "Rima" , "Mubassir"];
+for (i = 0 ; i < couple.length ; i++){
+    console.log(i);
+    console.log(typeof(i));
+    console.log(couple[i]);
+    console.log(typeof(couple[i]));
+}
