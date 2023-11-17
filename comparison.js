cc6f4e946fd494590836fa9bb216fbf31238b4a0
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-17 20:30:08 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,16 @@
+var x = 10 ;
+var y = 20 ;
+var z = 15 ;
+
+if (x > y){
+    console.log("x holo y ar theke boro");
+}
+console.log(" y boro");
+
+let age = 19;
+if (age >= 18){
+    console.log("You are adult !")
+} else {
+    console.log("You are not adult !")
+}
+
