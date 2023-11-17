4a288457dee54595be772fad1b6b8f5ab5c6ad93
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-17 23:42:56 +0600

    some class practice

--- comparison.js
+++ comparison.js
@@ -13,4 +13,12 @@ if (age >= 18){
 } else {
     console.log("You are not adult !")
 }
+// console.log ar vitor condition lekhew kora jai true / false-- eta ternary oparator..
+let adultAge = 18;
 
+console.log(age >= 18 ? "You are Adults" : "You are not Adults");
+
+// hard niyom:
+
+let young = 28;
+console.log(young <= 22 ? (young < 18 ) ? "You are child" : "You are adult" : "You are Young");
