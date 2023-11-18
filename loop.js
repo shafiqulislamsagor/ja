1ca0c845c46cb1df6fd4c632d9a295bfa79ec7d2
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-18 22:50:43 +0600

    some for loops

--- loop.js
+++ loop.js
@@ -8,4 +8,17 @@ for(let i = 0 ; cars.length >= i; i++){
 
 console.log("For loop done .")
 
+let i = 0 ;
+
+for(; ; i++){
+    if(i <= 4){
+        console.log(i);
+    }
+    else{
+        break;
+    }
+}
+
+console.log("i=0 and condition na dilew avhabe kora jai..")
+
 // while loops
