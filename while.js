2ff229bac6a30051fed15ef06704446137eeccce
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-19 00:51:10 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,22 @@
+let i = 0;
+text ="" ;
+
+while(i < 10){
+    text +="The number is " + i;
+    i++ ;
+
+}
+
+console.log(text);
+
+// similar work:
+
+let m = 20;
+write = "";
+
+do {
+    write += "this type " + m;
+    m++;
+} while (m < 15);
+
+console.log(write);
