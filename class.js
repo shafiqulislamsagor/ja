10a39dc8d056623ed46e26df2cbc85ce22d6f819
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-20 12:47:17 +0600

    class use javascript

---
+++ class.js
@@ -0,0 +1,14 @@
+class car {
+    constructor(name,year){
+        this.carName = name;
+        this.carYear = year;
+    }
+}
+
+const car1 = new car("Toyata",2017);
+const car2 = new car("BMW",2020);
+const car3 = new car("Suzuki",2023);
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
