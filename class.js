3a31f525a3ee2e110f8a2ee6f2d0d102d355c73e
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-20 13:13:26 +0600

    javascript class mathod

--- class.js
+++ class.js
@@ -12,3 +12,20 @@ const car3 = new car("Suzuki",2023);
 console.log(car1);
 console.log(car2);
 console.log(car3);
+
+
+class personAll {
+    constructor(name,age){
+        this.personName= name;
+        this.personAge = age;
+    }
+    play(stutas){
+        console.log(`${this.personName} is playing carrom pool ${stutas}`);
+    }
+}
+
+const person1 = new personAll("Sagor",18);
+const person2 = new personAll("Habiba",17);
+
+person1.play("Pro Player");
+person2.play("Boot Player");
