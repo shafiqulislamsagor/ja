e5e5eb0dd15af2d7184da5c1100bd6bdf309d7d2
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-20 10:53:42 +0600

    some javascript code

--- test/test.js
+++ test/test.js
@@ -1,3 +1,23 @@
 function time(){
     document.getElementById('is').innerHTML = Date();
 }
+
+x = 5;
+
+console.log(x);
+
+var x;// var ar jonno hoisting hoi///
+
+// object ar modde object thakle this use hoi:
+
+const person = {
+    firstName : "Shafiqul Islam",
+    lastName : "Sagor",
+    id : 220277 ,
+    fullName : function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log(person);
+
